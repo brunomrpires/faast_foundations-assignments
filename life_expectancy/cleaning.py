96aa7fcad57d6ab7e726987fb93b2ef79f90dc9a
Bruno Repolho Pires <brpires@corporativo.pt>
2022-11-04 12:03:50 +0000

    cleaning.py code refactoring

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -1,133 +1,110 @@
 """Module to clean assignment data"""
+
 import os
 import re
 import argparse
 import pandas as pd
+import numpy as np
 from pandas import DataFrame
 
 
-def load_raw_data(
-    raw_data_file_path: str
-):
+def load_data():
     """
     This function is responsible for loading the raw data,
     applying the correct split and tranforming it to a pandas dataframe
 
-    param: raw_data_file_path -> path to the raw data file
-
-    return: Pandas dataframe with columns properly defined
+    return: eu_life_expectancy_raw_dataframe -> pandas dataframe
     """
-    os_path_pardir = os.path.dirname(__file__)
+
+    #Get parent directory
+    parent_directory = os.path.dirname(__file__)
+    raw_data_file_path = '/data/eu_life_expectancy_raw.tsv'
 
     #Read tsv file
-    splitted_file = []
-    with open(os_path_pardir + raw_data_file_path, encoding="utf-8") as file:
-        delimiters = ',', '\\time\t', '\t', '\n'
-        regex_pattern = '|'.join(map(re.escape, delimiters))
-        #Read first line
+    file_with_split_lines = []
+    with open(parent_directory + raw_data_file_path,
+              encoding="utf-8") as file:
+
+        #Get list of file delimiters
+        delimiters = [',', '\\time\t', '\t', '\n']
+        delimiters_pattern = '|'.join(map(re.escape, delimiters))
+
+        #Split file line by line
         for line in file:
-            splitted_line = re.split(regex_pattern,line)
-            splitted_file.append(splitted_line[:-1])
+            splitted_line = re.split(delimiters_pattern, line)
+            file_with_split_lines.append(splitted_line[:-1]) #Exclude \n last split
 
-    eu_life_expectancy_raw_data = pd.DataFrame(splitted_file[1:], columns = splitted_file[0])
-
-
-    return eu_life_expectancy_raw_data
-
+    #Create pandas dataframe
+    eu_life_expectancy_raw_dataframe = pd.DataFrame(data = file_with_split_lines[1:],
+                                               columns = file_with_split_lines[0])
 
 
-def unpivot_date(
-    raw_dataframe: DataFrame
+    return eu_life_expectancy_raw_dataframe
+
+def clean_data(
+    raw_dataframe: DataFrame,
+    country: str = 'PT'
 ):
     """
-    This function unpivots the date to long format so that
-    the dataframe has the following columns: unit, sex, age, region, year, value
+    This function takes the raw data as a pandas dataframe
+    and returns the cleaned data as a pandas dataframe
 
-    param: raw_dataframe -> raw pandas DataFrame with pivoted date column
+    param: raw_dataframe -> raw data
+    param: country -> region to filter data by
 
-    return: pandas dataframe with date column unpivoted
+    return: clean_dataframe -> clean data
     """
-    #unpivoted_dataframe = pd.melt(raw_dataframe, id_vars='year')
 
-
+    #Unpivot dataframe
     unpivoted_dataframe = pd.melt(raw_dataframe,
-                                id_vars=['unit','sex','age',"geo"],
-                                var_name='year',
-                                value_name = 'value')
+                                  id_vars = ['unit','sex','age',"geo"],
+                                  var_name = 'year',
+                                  value_name = 'value')
 
-
-    return unpivoted_dataframe
-
-def set_and_check_data_types(
-    unpivoted_dataframe: DataFrame
-):
-    """
-    This function sets each columns datatypes according to the assignments specs and scheck them
-
-    param: unpivoted_dataframe -> pandas DataFrame after unpivotting date column
-
-    return: pandas dataframe with columns in the correct data type
-    """
-
-    data_types_dataframe = unpivoted_dataframe
-
+    #Set and check data types
+    dataframe_correct_dtypes = unpivoted_dataframe
     #Cast year column as integer
-    data_types_dataframe['year'] = data_types_dataframe['year'].astype('int')
+    dataframe_correct_dtypes['year'] = dataframe_correct_dtypes['year'].astype('int')
+    #Cast value column as float
+    dataframe_correct_dtypes['value'] = dataframe_correct_dtypes['value'].str.split(' ').str[0]
+    #Deal with null values
+    dataframe_correct_dtypes['value'] = dataframe_correct_dtypes['value'].replace({':': -1, ': ': -1})
+    dataframe_correct_dtypes['value'] = dataframe_correct_dtypes['value'].astype('float')
+    dataframe_correct_dtypes = dataframe_correct_dtypes[dataframe_correct_dtypes['value'] != -1]
+    #Rename columns
+    dataframe_correct_dtypes.columns = ['unit', 'sex', 'age', 'region', 'year', 'value']
 
-    #Cast value column as float (deal with null values)
-    data_types_dataframe['value'] = data_types_dataframe['value'].str.split(' ').str[0]
+    #Filter by region
+    clean_dataframe = dataframe_correct_dtypes[dataframe_correct_dtypes['region'] == country]
 
-    data_types_dataframe['value'] = data_types_dataframe['value'].replace({':': 0, ': ': 0})
-    data_types_dataframe['value'] = data_types_dataframe['value'].astype('float')
-    data_types_dataframe = data_types_dataframe[data_types_dataframe['value'] != 0]
-
-    data_types_dataframe.columns = ['unit', 'sex', 'age', 'region', 'year', 'value']
-
-    return data_types_dataframe
-
-def filter_by_region(
-    all_regions_dataframe: DataFrame,
-    region: str
-):
-    """
-    This function filters the provided dataframe by the specified region
-
-    :param all_regions_dataframe -> dataframe with every region's data
-    :param region -> region to filter the data by
-
-    return region_filtered_dataframe -> dataframe filtered by region
-    """
-
-    region_filtered_dataframe = all_regions_dataframe[all_regions_dataframe['region'] == region]
-
-    return region_filtered_dataframe
+    return clean_dataframe
 
 def save_data(
-    filtered_dataframe: DataFrame,
-    safe_path: str
+    clean_dataframe: DataFrame,
 ):
     """
     This function saves the final dataframe in the desired path
+    param: clean_dataframe -> Cleaned pandas dataframe
     """
-    os_path_pardir = os.path.dirname(__file__)
 
-    filtered_dataframe.to_csv(os_path_pardir + safe_path, index = False)
+    #Get parent directory
+    parent_directory = os.path.dirname(__file__)
+    save_data_path = '/data/pt_life_expectancy.csv'
 
-def clean_data(country: str = 'PT'):
+    clean_dataframe.to_csv(parent_directory + save_data_path,
+                           index = False)
+
+def main(country : str = 'PT'):
     """
-    This function performs the full data cleaning process
+    This function should load the data, clean it and save it
+
+    param: country -> region to filter the data by
     """
-    raw_data_file_path = '/data/eu_life_expectancy_raw.tsv'
-    final_data_file_path = '/data/pt_life_expectancy.csv'
-    raw_dataframe = load_raw_data(raw_data_file_path)
+    raw_dataframe = load_data()
+    clean_dataframe = clean_data(raw_dataframe = raw_dataframe,
+                                 country = country)
+    save_data(clean_dataframe = clean_dataframe)
 
-    unpivoted_dataframe = unpivot_date(raw_dataframe)
-
-    data_types_dataframe = set_and_check_data_types(unpivoted_dataframe)
-
-    region_diltered_dataframe = filter_by_region(data_types_dataframe, region = country)
-
-    save_data(region_diltered_dataframe, final_data_file_path)
 
 
 if __name__ == "__main__": # pragma: no cover
@@ -136,4 +113,4 @@ if __name__ == "__main__": # pragma: no cover
                         help='Country to choose')
 
     args = parser.parse_args()
-    clean_data(args.country)
+    main(args.country)
